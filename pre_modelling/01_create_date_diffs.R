7f27bb41b33a4ab81868e1fb4bbab3957453474a
Lachlan McLachlan <lachlan.mclachlan@outlook.com>
2017-07-27 10:21:51 +0100

    Create date difference function and build fist model for feature selection

--- pre_modelling/01_create_date_diffs.R
+++ pre_modelling/01_create_date_diffs.R
@@ -2,58 +2,48 @@
 library(tidyverse)
 library(lubridate)
 library(stringr)
-# POSITIVES ---------------------------------------------------------------
-# Convert all dates to correct format:
-Pos_dates <- read_rds(paste0("F:/Projects/Strongbridge/data/Cohorts/01_Cohorts_by_variable_type/",
-                             "Pos_dates",
-                             ".rds"))
-# missing values:
-Miss_Pos_dates <- prop_missing(Pos_dates)
-colnames(Pos_dates)[Miss_Pos_dates == 1]
-table(str_sub(colnames(Pos_dates), -5, -1))
-Pos_dt_exp <-  select(Pos_dates, ends_with("EXP_"))
+
+# 1. READ IN DATA
+Date_data <- read_rds(paste0("PATH",
+                             "TO",
+                             "FILE"))
 
 
-Pos_date_format <- date_format(input_data = Pos_dates, date_pattern = "EXP_DT",
+# 2. FORMAT DATE DATA. Note this doesn't yet work for the 84 date variales in the
+# procedures data because the date formate is different. But it does work for the
+# 3192 other date vaiables.
+formatted_dates <- date_format(input_data = Date_data, date_pattern = "EXP_DT",
                                PATIENT_ID_col = "PATIENT_ID")
 
-Pos_date_format$looback_days <- mdy(Pos_all$final_lookback)
-Pos_date_format$index_date <- mdy(Pos_all$final_index)
+# 3. APPEND THE INDEX DATE COLUNM
+formatted_dates$index_date <- mdy(Date_data$index_date)
 
-# check that none of these dates occur before the lookback date:
-# These are actually the date difference variables:
-Date_diffs_pos_df <- as.data.frame(sapply(Pos_date_format[,-1], function(x) { 
-  Pos_date_format$index_date - x
-}))
+# 4. CREATE THE DATE DIFFERENCES USING THE FOLLOWING FUNCTION
+# first set patient_id to NULL. You will need to cbind patient id
+# to the date differences after you have created them.
+formatted_dates$PATIENT_ID <- NULL
 
-# NEGATIVES ---------------------------------------------------------------
+date_diffs <- create_date_diffs(input = formatted_dates, index_col = "index_date")
 
-Neg_dates <- read_rds(paste0("F:/Projects/Strongbridge/data/Cohorts/01_Cohorts_by_variable_type/",
-                             "Neg_dates",
-                             ".rds"))
-
-Neg_date_format <- date_format(input_data = Neg_dates[,c(1, 4:ncol(Neg_dates))], date_pattern = "EXP_DT",
-                               PATIENT_ID_col = "PATIENT_ID")
-Neg_date_format$lookback_date <- mdy(Neg_dates$lookback_date)
-Neg_date_format$index_date <- mdy(Neg_dates$index_date)
-# check that none of these dates occur before the lookback date:
-# These are actually the date difference variables:
-Date_diffs_neg_df <- as.data.frame(sapply(Neg_date_format[,-1], function(x) { 
-  Neg_date_format$index_date - x
-  }))
-min(Date_diffs_neg_df, na.rm = TRUE)
-
-# compare the lookback date to the first and last exposure dates:
-neg_lookback_diff <- as.data.frame(sapply(Neg_date_format[,-1], function(x) {
-  x - Neg_date_format$looback_date
-}))
-min(neg_lookback_diff, na.rm = TRUE)
-sapply(neg_lookback_diff, class)
 
 # FUNCTIONS ---------------------------------------------------------------
 
+# create date diff vars:
+
+# please input only a dataframe full of dates into this function:
+create_date_diffs <- function(input, index_col = "index_date") {
+  
+  date_cols <- input[, -which(colnames(input) == index_col)]
+  
+  date_diffs <- as.data.frame(sapply(date_cols, function(x) { 
+    input[[index_col]] - x
+    
+  }))
+  
+  return(date_diffs)
+}
+
 # convert date format:
-#
 date_format <- function(input_data, date_pattern, PATIENT_ID_col) {
   date_data <- select(input_data, contains(date_pattern))
   formatted <- lapply(date_data, mdy)
@@ -62,16 +52,4 @@ date_format <- function(input_data, date_pattern, PATIENT_ID_col) {
                    df_date
   )
   return(df)
-}
-
-#  proportion missing for each variable:
-prop_missing <- function(input_data) {
-  
-  prop_miss <- sapply(input_data, function(x) {
-    
-    sum(is.na(x))/nrow(input_data)
-    
-  })
-  print(summary(prop_miss))
-  return(prop_miss)
 }
--- pre_modelling/feature_selection/04_XGBoost_importance.R
+++ pre_modelling/feature_selection/04_XGBoost_importance.R
@@ -6,33 +6,55 @@ library(mrl)
 library(tidyverse)
 library(stringr)
 library(palabmod)
+library(xgboost)
 
 
 # DATA IN -----------------------------------------------------------------
 
 Pos_freq <- read_rds(paste0("F:/Projects/Strongbridge/data/Cohorts/01_Cohorts_by_variable_type/Feat_selection/",
                                                   "Pos_common_frequencies_FS",
-                                                  ".rds")))
+                                                  ".rds"))
 Neg_freq <- read_rds(paste0("F:/Projects/Strongbridge/data/Cohorts/01_Cohorts_by_variable_type/Feat_selection/",
                             "Neg_common_frequencies_FS",
-                            ".rds")))
+                            ".rds"))
 
 
-# MANIPULATION ------------------------------------------------------------
-
+# PREMODELLING ------------------------------------------------------------
+# MANIPULATION 
+Neg_freq$test_patient_id <- NULL
 # Order variables so the datasets can be bound
-all.equal(colnames(Pos_freq), colnames(Neg_freq))
+setdiff(colnames(Pos_freq), colnames(Neg_freq))
+Pos_order <- Pos_freq[order(match(colnames(Pos_freq), colnames(Neg_freq)))]
+all.equal(colnames(Pos_order), colnames(Neg_freq))
 
+# convert all freq variables to numeric:
+Neg_freq[,8:ncol(Neg_freq)] <- sapply(Neg_freq[,8:ncol(Neg_freq)], as.numeric)
+Pos_order[,8:ncol(Pos_order)] <- sapply(Pos_order[,8:ncol(Pos_order)], as.numeric)
+combined_data <- bind_rows(Neg_freq, Pos_order)
 
+# create gender dummy
+combined_data$gender_dum <- ifelse(combined_data$GENDER == "F", 1, 0)
+# remove non-modelling variables:
+modelling_data <- combined_data
+modelling_data$PATIENT_ID <- NULL
+modelling_data$GENDER <- NULL
+modelling_data$index_date <- NULL
+modelling_data$lookback_date <- NULL
+# D_3592 is the dirty PPP code. Therefore it shouldn't be included in the
+# modelling data. Nor should lookback in days
+modelling_data$D_3592_AVG_CLAIM_CNT <- NULL
+modelling_data$lookback_days <- NULL
+modelling_data$label <- as.factor(modelling_data$label)
 # MODELLING ---------------------------------------------------------------
 
+table(combined_data$label)
 # Make modelling dataset
 model_data <- makeClassifTask(id = "Feature_selection", 
-                              data = combined_data, 
+                              data = modelling_data, 
                               target = "label",
                               positive = 1)
 
-lrn_xgb <- makeLearner("classif.xgboost"
+lrn_xgb <- makeLearner("classif.xgboost",
                        predict.type = "prob")
 
 lrn_xgb$par.vals = list(
@@ -52,7 +74,11 @@ pr_resam <- palabmod::perf_binned_perf_curve(resam$pred,
                                              y_metric = "prec",
                                              agg_func = mean)
 
+write_csv(pr_resam$curve, "F:/Projects/Strongbridge/results/feature_selection/01_XGBoost_freq.csv")
 
+xgb_model <- train(learner = lrn_xgb, task = model_data)
+  
+importance <- xgb.importance(model = xgb_model$learner.model, feature_names = xgb_model$features)
 
 
 
